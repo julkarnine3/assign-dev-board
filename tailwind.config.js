ea51c52d1c0f3c810b3664a04daf28a0478a0229
julkarnine3 <julkarninemdtarek@gmail.com>
2025-04-21 05:07:30 +0600

    Added activity log section

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"], // update paths as needed
+    theme: {
+      extend: {
+        fontFamily: {
+          poppins: ['Poppins', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
